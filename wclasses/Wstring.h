120dca74f1ac4bac9db9bf049c1f49fbc2b42b39
J.P. Iivonen <wondermacros@yahoo.com>
2019-05-11 20:33:52 +0300

    Add initial Wstring class.

---
+++ wclasses/Wstring.h
@@ -0,0 +1,9 @@
+#ifndef __WCLASSES_WSTRING_H
+#define __WCLASSES_WSTRING_H
+
+#include "forward_declare.h"
+#include "wclasses.h"
+#include "Wstring_class.h"
+#include <wondermacros/objects/x/class_declare.h>
+
+#endif /* Wstring */
