a386c0232df5ac5cff1f45449e1c0b8bd3c17675
J.P. Iivonen <wondermacros@yahoo.com>
2019-05-12 18:08:25 +0300

    Add more string functions.

--- wclasses/WstringRope_class.h
+++ wclasses/WstringRope_class.h
@@ -3,6 +3,6 @@
 
 #define WstringRope__define \
     VAR(read,struct wstring_buffer*,bufs) \
-    METHOD(WstringRope,public,void,prepend,(const char* buf, size_t len)) \
     METHOD(WstringRope,public,void,append,(const char* buf, size_t len)) \
+    METHOD(WstringRope,public,void,prepend,(const char* buf, size_t len)) \
     /**/
